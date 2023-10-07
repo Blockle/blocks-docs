46d65887aaf586677e00ef9bfebc5c01e29fe482
Niek Saarberg <niek@Nieks-MacBook-Air.fritz.box>
2023-10-07 21:25:05 +0200

    Ugly ts error workaround

--- src/components/SyntaxHighlighter/SyntaxHighlighter.tsx
+++ src/components/SyntaxHighlighter/SyntaxHighlighter.tsx
@@ -1,6 +1,12 @@
 // Funcky import, see https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/493
+import { FC } from 'react';
 import ReactSyntaxHighlighter from 'react-syntax-highlighter/dist/esm/default-highlight';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+// Typing issue.. tmp workaround
+const Highlighter = ReactSyntaxHighlighter as unknown as FC<
+  SyntaxHighlighterProps & { style: Record<string, React.CSSProperties> }
+>;
 
 export type SyntaxHighlighterProps = {
   children: string;
@@ -9,8 +15,8 @@ export type SyntaxHighlighterProps = {
 
 export const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = ({ children, language }) => {
   return (
-    <ReactSyntaxHighlighter language={language} style={docco}>
+    <Highlighter language={language} style={docco}>
       {children}
-    </ReactSyntaxHighlighter>
+    </Highlighter>
   );
 };
