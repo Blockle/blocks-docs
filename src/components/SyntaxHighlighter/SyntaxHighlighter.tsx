0b2c8215f07aa706a6e2be1628e3690ce19c5850
nsaarberg <nsaarberg@xebia.com>
2025-10-02 14:39:48 +0200

    Removed react-syntax-highlighter

--- src/components/SyntaxHighlighter/SyntaxHighlighter.tsx
+++ src/components/SyntaxHighlighter/SyntaxHighlighter.tsx
@@ -1,14 +1,6 @@
-// Funcky import, see https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/493
-import { Prism } from 'react-syntax-highlighter';
-// import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
-import { ghcolors } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
 export type SyntaxHighlighterProps = { children: string; language: string };
 
+// TODO Geimplemnt syntax highlighting
 export const SyntaxHighlighter: React.FC<SyntaxHighlighterProps> = ({ children, language }) => {
-  return (
-    <Prism language={language} style={ghcolors}>
-      {children}
-    </Prism>
-  );
+  return <pre>{children}</pre>;
 };
